5be2ca9dcd9f76ea9b73dde5ec93ac7952ae4ca0
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2015-05-14 19:55:06 -0300

    Buscando Stubs do WSRemoto através do wsimport
    Signed-off-by: Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
    
    No ramo master
    Mudanças a serem submetidas:
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/ConversionRate.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/ConversionRateResponse.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/Currency.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/CurrencyConvertor.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/CurrencyConvertorSoap.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/ObjectFactory.java
    	new file:   Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/package-info.java

---
+++ Source_Code/src/main/java/br/ufg/si/ad/webServiceTest/stubs/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.webserviceX.NET/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.ufg.si.ad.webServiceTest.stubs;
